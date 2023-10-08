39624ac4eed2a6cb8158b2e175d588927005d549
Riccardo Potalivo <riccardopotalivo@gmail.com>
2023-10-08 10:02:06 +0200

    bonus-complete

--- js/script.js
+++ js/script.js
@@ -5,9 +5,25 @@ const numeroFisso = 21
 
 let password =
 `
-    La tua password è ${nome}${cognome}${colorePreferito}${numeroFisso}
+    ${nome}${cognome}${colorePreferito}${numeroFisso}
 `
 
 console.log(nome);
 
-document.getElementById('text').innerHTML = password;
+document.getElementById("text").value = password;
+
+
+function myFunction() {
+    // Get the text field
+    var copyText = document.getElementById("text");
+  
+    // Select the text field
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); // For mobile devices
+  
+     // Copy the text inside the text field
+    navigator.clipboard.writeText(copyText.value);
+  
+    // Alert the copied text
+    alert("Copied the text: " + copyText.value);
+  }
